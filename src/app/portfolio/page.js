73bfc8252039f344650a3f2f18df1826f6e173a0
Naim Ahmad <naimahmad201103@gmail.com>
2025-01-26 17:49:25 +0600

    converted all page to next js

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -12,73 +12,73 @@ export default function Portfolio() {
     <>
       <Breadcrumb title="Portfolio" />
       <main>
-        <div class="container">
-          <section class="projects-section">
+        <div className="container">
+          <section className="projects-section">
             <h2>We&#39;re More Than Just A Number</h2>
-            <p class="sec-des">
+            <p className="sec-des">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam.
             </p>
-            <div class="projects-grid">
-              <div class="project-card">
+            <div className="projects-grid">
+              <div className="project-card">
                 <Image src={product1} alt="Project 1" />
-                <div class="project-info">
+                <div className="project-info">
                   <h3>Project Title</h3>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   </p>
-                  <button class="view-project-btn">View Project</button>
+                  <button className="view-project-btn">View Project</button>
                 </div>
               </div>
-              <div class="project-card">
+              <div className="project-card">
                 <Image src={product2} alt="Project 2" />
-                <div class="project-info">
+                <div className="project-info">
                   <h3>Project Title</h3>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   </p>
-                  <button class="view-project-btn">View Project</button>
+                  <button className="view-project-btn">View Project</button>
                 </div>
               </div>
-              <div class="project-card">
+              <div className="project-card">
                 <Image src={product3} alt="Project 3" />
-                <div class="project-info">
+                <div className="project-info">
                   <h3>Project Title</h3>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   </p>
-                  <button class="view-project-btn">View Project</button>
+                  <button className="view-project-btn">View Project</button>
                 </div>
               </div>
-              <div class="project-card">
+              <div className="project-card">
                 <Image src={product4} alt="Project 4" />
-                <div class="project-info">
+                <div className="project-info">
                   <h3>Project Title</h3>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   </p>
-                  <button class="view-project-btn">View Project</button>
+                  <button className="view-project-btn">View Project</button>
                 </div>
               </div>
-              <div class="project-card">
+              <div className="project-card">
                 <Image src={product5} alt="Project 5" />
-                <div class="project-info">
+                <div className="project-info">
                   <h3>Project Title</h3>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   </p>
-                  <button class="view-project-btn">View Project</button>
+                  <button className="view-project-btn">View Project</button>
                 </div>
               </div>
-              <div class="project-card">
+              <div className="project-card">
                 <Image src={product6} alt="Project 6" />
-                <div class="project-info">
+                <div className="project-info">
                   <h3>Project Title</h3>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   </p>
-                  <button class="view-project-btn">View Project</button>
+                  <button className="view-project-btn">View Project</button>
                 </div>
               </div>
             </div>
--- src/app/reviews/page.js
+++ src/app/reviews/page.js
@@ -1,3 +1,62 @@
+import r1 from "@/assets/images/r-1.jpeg";
+import Image from "next/image";
+
 export default function Reviews() {
-  return <div>Reviews</div>;
+  return (
+    <>
+      <main className="container mt-5">
+        <div className="testimonial-card row gap-3 gap-md-0">
+          <div className="col-md-7">
+            <div className="stars">&#9733; &#9733; &#9733; &#9733; &#9733;</div>
+            <h5 className="mb-3">Mrs D Parry</h5>
+            <p className="testimonial-text">
+              Land Professionals came to my attention through a letter, I had
+              previously spoken to many developers, as I was aware my garden had
+              value. I found Phil & Ella to be personable and patient, which
+              suited me best. I never felt pushed and was overjoyed at the
+              amount I was offered. The transaction went smoothly and within the
+              agreed time frame.
+            </p>
+          </div>
+          <div className="col-md-5">
+            <Image src={r1} alt="Testimonial" className="testimonial-image" />
+          </div>
+        </div>
+        <div className="testimonial-card row gap-3 gap-md-0">
+          <div className="col-md-7">
+            <div className="stars">&#9733; &#9733; &#9733; &#9733; &#9733;</div>
+            <h5 className="mb-3">Mrs D Parry</h5>
+            <p className="testimonial-text">
+              Land Professionals came to my attention through a letter, I had
+              previously spoken to many developers, as I was aware my garden had
+              value. I found Phil & Ella to be personable and patient, which
+              suited me best. I never felt pushed and was overjoyed at the
+              amount I was offered. The transaction went smoothly and within the
+              agreed time frame.
+            </p>
+          </div>
+          <div className="col-md-5">
+            <Image src={r1} alt="Testimonial" className="testimonial-image" />
+          </div>
+        </div>
+        <div className="testimonial-card row gap-3 gap-md-0">
+          <div className="col-md-7">
+            <div className="stars">&#9733; &#9733; &#9733; &#9733; &#9733;</div>
+            <h5 className="mb-3">Mrs D Parry</h5>
+            <p className="testimonial-text">
+              Land Professionals came to my attention through a letter, I had
+              previously spoken to many developers, as I was aware my garden had
+              value. I found Phil & Ella to be personable and patient, which
+              suited me best. I never felt pushed and was overjoyed at the
+              amount I was offered. The transaction went smoothly and within the
+              agreed time frame.
+            </p>
+          </div>
+          <div className="col-md-5">
+            <Image src={r1} alt="Testimonial" className="testimonial-image" />
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
