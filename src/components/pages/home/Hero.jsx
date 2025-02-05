3abf1bd09f7811972f1143925fcc0279c3e0b0cc
Naim Ahmad <naimahmad201103@gmail.com>
2025-02-06 02:17:59 +0600

    updated hero section

--- src/components/pages/home/Hero.jsx
+++ src/components/pages/home/Hero.jsx
@@ -1,25 +1,69 @@
 
-import slider1 from "@/assets/images/slider-1.jpg";
+"use client"
+
+import slider1 from "@/assets/images/hero-slider-1.jpg";
+import slider2 from "@/assets/images/hero-slider-2.jpg";
+import slider3 from "@/assets/images/hero-slider-3.jpg";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const sliders = [
+  {
+    image: slider1,
+    title: `Build Your <span class="heigh-light">Dream Home</span> <br />
+      Reach Us, and we will do the rest`,
+    caption: `Beautiful Lake View`,
+  },
+  {
+    image: slider2,
+    title: `Build Your <span class="heigh-light">Dream Home</span> <br />
+      Reach Us, and we will do the rest`,
+    caption: `Amazing Street View`,
+  },
+  {
+    image: slider3,
+    title: `Sell Your land With <span class="heigh-light"> maximized value.</span>`,
+    caption: `Land Picture`,
+  },
+
+]
 
 export default function Hero() {
 
+  const [active, setActive] = useState(0)
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActive((prev) => prev === sliders.length - 1 ? 0 : prev + 1)
+    }, 5000)
+    return () => clearInterval(interval)
+  }, [])
+
+
+  const handleClick = (value) => {
+    setProgress(value)
+    setActive(value)
+  }
+
+  console.log(progress)
+
   return (
     <div className="hero-home">
+      {/* <!-- slider progress --> */}
+      <div className="slider-progress">
+        <div
+          className={`progress-bar `}
+          role="progressbar"
+          style={{ width: `${100}%` }}
+        ></div>
+      </div>
 
       {/* <!-- slider image --> */}
       <div className="slider-image-div">
-        {/* <!-- slider progress --> */}
-        <div className="slider-progress">
-          <div
-            className="progress-bar"
-            role="progressbar"
-            style={{ width: "100%" }}
-          ></div>
-        </div>
         <Image
-          src={slider1}
+          src={sliders[active].image}
           alt="placeholder"
           quality={100}
           placeholder="blur"
@@ -33,9 +77,10 @@ export default function Hero() {
           <div className="card real-estate-card">
             <div className="card-body">
               {/* <h5 className="card-price">$1 200 000</h5> */}
-              <h4 className="card-title">
-                Unlock the <span className="heigh-light">Hidden Value</span> <br />
-                 of Your Land
+              <h4 className="card-title" dangerouslySetInnerHTML={{
+                __html: sliders[active].title,
+              }}>
+
               </h4>
               <p className="card-text">
                 Build Your Dream Home, Reach Us, and we will do the rest.
@@ -52,9 +97,16 @@ export default function Hero() {
 
       {/* <!-- slider pagination --> */}
       <div className="slider-pagination">
-        <span className="slider-pagination-item active"></span>
-        <span className="slider-pagination-item"></span>
-        <span className="slider-pagination-item"></span>
+        {
+          sliders.map((slide, index) => (
+            <span
+              key={index}
+              className={`slider-pagination-item ${index === active ? "active" : ""}`}
+              onClick={() => handleClick(index)}
+            ></span>
+          ))
+        }
+
       </div>
 
     </div>
