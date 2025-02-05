977ad4f6ce461b1edd7ccafcb2bc483067f36415
Naim Ahmad <naimahmad201103@gmail.com>
2025-02-05 17:38:46 +0600

    about section completed

--- src/components/pages/home/About.jsx
+++ src/components/pages/home/About.jsx
@@ -1,6 +1,8 @@
-import about from "@/assets/images/about.jpg"
-import mission from "@/assets/images/mission.jpeg"
-import Image from "next/image"
+import about from "@/assets/images/about.jpg";
+import commitment from "@/assets/images/commitment.jpg";
+import mission from "@/assets/images/mission.jpeg";
+import Image from "next/image";
+import Link from "next/link";
 export default function About() {
 
   return (
@@ -15,15 +17,11 @@ export default function About() {
             />
           </div>
           <div className="col-md-6">
-            <h2 className="section-title">About Our Real Estate Agency</h2>
+            <h2 className="section-title">About Us & Who We Are</h2>
             <p>
-              We are a dynamic, innovative, and dedicated agency that
-              specializes in providing the best possible solutions to our
-              clients' real estate needs. Our team is made up of experts in
-              various fields, including architecture, interior design, and
-              property management.
+              We are a dynamic and innovative land brokerage firm committed to providing the best solutions for buying and selling land. With a team of experts in real estate, land development, and property management, we help clients navigate the market with confidence, ensuring seamless transactions and maximum value.
             </p>
-            <a href="#" className="ll-btn-primary">Learn More</a>
+            <Link href="/about" className="ll-btn-primary">Learn More</Link>
           </div>
         </div>
 
@@ -38,25 +36,44 @@ export default function About() {
           <div className="col-md-6">
             <h2 className="section-title">Mission and vision</h2>
             <p>
-              We are a dynamic, innovative, and dedicated agency that
-              specializes in providing the best possible solutions to our
-              clients' real estate needs. Our team is made up of experts in
-              various fields, including architecture, interior design, and
-              property management.
+              At Land & Living, our vision is to provide exceptional service by prioritizing strong relationships and community impact. We carefully assess each plot’s suitability, considering landowners' goals and the broader community’s needs.
             </p>
+            <p>
+              With a trusted network across the property development industry, we leverage deep market insights to evaluate every site effectively. Our commitment is to collaborate closely with landowners and agents across the UK to create sustainable developments where people want to live.
+            </p>
+            <p>
+              We work diligently with vendors to maximize land value while tailoring transactions to align with their unique needs and aspirations.
+            </p>
+
+          </div>
+        </div>
+
+        <div className="row align-items-center">
+          <div className="col-md-6 mb-5">
+            <Image
+              src={commitment}
+              alt="placeholder"
+              className="about-img"
+            />
+          </div>
+          <div className="col-md-6">
             <div>
-              <h2 className="mb-3">Our Commitment</h2>
+              <h2 className="mb-3 section-title">Our Commitment</h2>
               <p className="commitment">
-                <strong>Client Satisfaction:</strong>
+                <strong><u>Client Satisfaction</u> : </strong>
                 We prioritize client satisfaction by understanding their needs
                 and delivering beyond expectations.
               </p>
               <p className="commitment">
-                <strong>Top-Notch Services:</strong>
-                Our commitment is to provide exceptional, high-quality services
-                with precision and reliability.
+                <strong><u>Top-Notch Services</u> : </strong>
+                Our commitment is to provide exceptional, high-quality services with precision and reliability.
+              </p>
+              <p className="commitment">
+                <strong><u>Maximizing Land Value </u>: </strong>
+                we are committed to ensuring our clients receive the maximum value for their land. We carefully assess each site, leveraging market expertise and industry connections to secure the best possible deal. Our goal is to create a seamless and rewarding experience tailored to each landowner's needs.
               </p>
             </div>
+            {/* <Link href="/about" className="ll-btn-primary">Learn More</Link> */}
           </div>
         </div>
       </div>
--- src/components/pages/home/Hero.jsx
+++ src/components/pages/home/Hero.jsx
@@ -1,64 +1,62 @@
 
 import slider1 from "@/assets/images/slider-1.jpg";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Hero() {
 
   return (
     <div className="hero-home">
-        <div className="container">
-          {/* <!-- slider image --> */}
-          <div className="slider-image-div">
-            {/* <!-- slider progress --> */}
-            <div className="slider-progress">
-              <div
-                className="progress-bar"
-                role="progressbar"
-                style={{ width: "100%" }}
-              ></div>
+
+      {/* <!-- slider image --> */}
+      <div className="slider-image-div">
+        {/* <!-- slider progress --> */}
+        <div className="slider-progress">
+          <div
+            className="progress-bar"
+            role="progressbar"
+            style={{ width: "100%" }}
+          ></div>
+        </div>
+        <Image
+          src={slider1}
+          alt="placeholder"
+          quality={100}
+          placeholder="blur"
+          priority
+        />
+      </div>
+
+      {/* <!-- hero content --> */}
+      <div className="container position-relative h-100">
+        <div className="hero-content">
+          <div className="card real-estate-card">
+            <div className="card-body">
+              {/* <h5 className="card-price">$1 200 000</h5> */}
+              <h4 className="card-title">
+                Unlock the <span className="heigh-light">Hidden Value</span> <br />
+                 of Your Land
+              </h4>
+              <p className="card-text">
+                Build Your Dream Home, Reach Us, and we will do the rest.
+              </p>
+              <Link href="#" className="ll-btn-primary">
+                Contact Us
+              </Link>
             </div>
-            <Image src={slider1} alt="placeholder" quality={100} placeholder="blur" priority/>
-          </div>
 
-          {/* <!-- hero content --> */}
-          <div className="hero-content">
-            <div className="card real-estate-card">
-              <div className="card-body">
-                <h5 className="card-price">$1 200 000</h5>
-                <h4 className="card-title">550 W 51 Te, Miami Beach FL</h4>
-                <p className="card-text">
-                  What can be more exciting than a combination of modern
-                  architecture with traditional supplies and techniques?
-                </p>
-              </div>
 
-              <div className="card-footer">
-                <div className="card-details">
-                  <span>
-                    <i className="bi bi-house-door"></i> 3
-                  </span>
-                  <span>
-                    <i className="bi bi-bathtub"></i> 2
-                  </span>
-                  <span>
-                    <i className="bi bi-car-front"></i> 2
-                  </span>
-                </div>
-                <p className="card-size">1 689 Sq Ft</p>
-                <a href="#" className="btn btn-warning card-btn">
-                  DETAILS
-                </a>
-              </div>
-            </div>
-          </div>
-
-          {/* <!-- slider pagination --> */}
-          <div className="slider-pagination">
-            <span className="slider-pagination-item active"></span>
-            <span className="slider-pagination-item"></span>
-            <span className="slider-pagination-item"></span>
           </div>
         </div>
       </div>
+
+      {/* <!-- slider pagination --> */}
+      <div className="slider-pagination">
+        <span className="slider-pagination-item active"></span>
+        <span className="slider-pagination-item"></span>
+        <span className="slider-pagination-item"></span>
+      </div>
+
+    </div>
   )
 }
