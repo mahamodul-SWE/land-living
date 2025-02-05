8edd1e838962e9e80c36d6578f174f823d2b7c52
Naim Ahmad <naimahmad201103@gmail.com>
2025-02-06 02:54:25 +0600

    fixed home section

--- src/components/pages/home/ShortReviews.jsx
+++ src/components/pages/home/ShortReviews.jsx
@@ -1,5 +1,4 @@
 
-import test4 from "@/assets/images/test4.png"
 import bg from "@/assets/images/testimonial-banner-min.jpg"
 import Image from "next/image"
 
@@ -22,37 +21,38 @@ export default function ShortReviews() {
       />
       <div className="container">
         <div className="section-top">
-          <h3 className="section-title">Testimonials</h3>
+          <h3 className="section-title">Success Stories from Our Clients</h3>
           <p className="section-subtitle">The only thing matters to us!</p>
         </div>
         <div className="row">
           <div className="col-lg-6">
             <div className="testimonial-item">
               <figure>
-                <Image src={test4} alt="user1" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                </svg>
               </figure>
-              <p className="user-name">John Doe</p>
-              <p className="profession">Doctor</p>
+              <p className="user-name">James R. – Smooth & Profitable Experience</p>
+              {/* <p className="profession">Doctor</p> */}
               <p className="user-review">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                euismod, arcu id viverra tristique, quam velit semper dolor, id
-                maximus enim velit non velit. Donec vel turpis at arcu
-                condimentum pulvinar.
+                "Selling my land to Land Profits was a seamless experience. Their team handled everything with transparency and professionalism, ensuring I got the best value for my property. The entire process was stress-free, and I felt confident in every step. Highly recommend!"
               </p>
             </div>
           </div>
           <div className="col-lg-6">
             <div className="testimonial-item">
               <figure>
-                <Image src={test4} alt="user1" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                </svg>
               </figure>
-              <p className="user-name">John Doe</p>
-              <p className="profession">Doctor</p>
+              <p className="user-name">Sarah L. – Exceptional Service & Fair Pricing</p>
+              {/* <p className="profession">Doctor</p> */}
               <p className="user-review">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                euismod, arcu id viverra tristique, quam velit semper dolor, id
-                maximus enim velit non velit. Donec vel turpis at arcu
-                condimentum pulvinar.
+              "I was unsure about selling my land, but Land Profits guided me through the process with expert advice and market insights. They offered a fair price and took care of all the paperwork, making the transaction quick and hassle-free. A truly outstanding team!"
+
               </p>
             </div>
           </div>
